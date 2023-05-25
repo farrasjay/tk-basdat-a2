d0381aa31dcaca9d3d5745778a889937a960659e
Farras Hafizhudin <farras.hafizhudin@ui.ac.id>
2023-05-25 22:46:23 +0700

    register impl updates

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from utils.db_utils import dict_fetch_all
 from utils.users import get_user_role
+import uuid
 
 # Create your views here.
 def landing_page(request):
@@ -14,15 +15,6 @@ def login_page(request):
 def register_page(request):
     return render(request, "register.html")
 
-def register_panitia_page(request):
-    return render(request, "register_panitia.html")
-
-def register_manager_page(request):
-    return render(request, "register_manager.html")
-
-def register_penonton_page(request):
-    return render(request, "register_penonton.html")
-
 def dashboard_panitia_page(request):
     return render(request, "dashboard_panitia.html")
 
@@ -32,8 +24,88 @@ def dashboard_manajer_page(request):
 def dashboard_penonton_page(request):
     return render(request, "dashboard_penonton.html")
 
-def register_user(request):
-    return
+def register_panitia_page(request):
+    if request.method == 'POST':
+        newPanitiaUUID = uuid.uuid4()
+        username = request.POST.get("usernameField")
+        password = request.POST.get("passwordField")
+        nama_depan = request.POST.get("firstNameField")
+        nama_belakang = request.POST.get("lastNameField")
+        nomor_hp = request.POST.get("phoneNumberField")
+        email = request.POST.get("emailField")
+        alamat = request.POST.get("addressField")
+        status = request.POST.get("statusField")
+        jabatan = request.POST.get("jabatanField")
+
+        with connection.cursor() as cursor:
+            cursor.execute("SET SEARCH_PATH TO ULEAGUE;")
+            cursor.execute(f"""
+                INSERT INTO USER_SYSTEM VALUES ('{username}','{password}');
+                INSERT INTO NON_PEMAIN (id_non_pemain, nama_depan, nama_belakang, nomor_hp, email, alamat) VALUES ('{newPanitiaUUID}', '{nama_depan}', '{nama_belakang}', '{nomor_hp}', '{email}', '{alamat}');
+                INSERT INTO STATUS_NON_PEMAIN (id_non_pemain, status) VALUES ('{newPanitiaUUID}', '{status}');
+                INSERT INTO PANITIA (id_panitia, jabatan, username) VALUES ('{newPanitiaUUID}', '{jabatan}', '{username}');
+            """)
+
+            response = HttpResponse()
+            response.status_code = 200
+
+        return redirect('authentication:login_page')
+    return render(request, "register_panitia.html")
+
+def register_manager_page(request):
+    if request.method == 'POST':
+        print("MASUK REG MANAJER")
+        newManajerUUID = uuid.uuid4()
+        username = request.POST.get("usernameField")
+        password = request.POST.get("passwordField")
+        nama_depan = request.POST.get("firstNameField")
+        nama_belakang = request.POST.get("lastNameField")
+        nomor_hp = request.POST.get("phoneNumberField")
+        email = request.POST.get("emailField")
+        alamat = request.POST.get("addressField")
+        status = request.POST.get("statusField")
+
+        with connection.cursor() as cursor:
+            cursor.execute("SET SEARCH_PATH TO ULEAGUE;")
+            cursor.execute(f"""
+                INSERT INTO USER_SYSTEM VALUES ('{username}','{password}');
+                INSERT INTO NON_PEMAIN (id_non_pemain, nama_depan, nama_belakang, nomor_hp, email, alamat) VALUES ('{newManajerUUID}', '{nama_depan}', '{nama_belakang}', '{nomor_hp}', '{email}', '{alamat}');
+                INSERT INTO STATUS_NON_PEMAIN (id_non_pemain, status) VALUES ('{newManajerUUID}', '{status}');
+                INSERT INTO MANAJER (id_manajer, username) VALUES ('{newManajerUUID}', '{username}');
+            """)
+
+            response = HttpResponse()
+            response.status_code = 200
+
+        return redirect('authentication:login_page')
+    return render(request, "register_manager.html")
+
+def register_penonton_page(request):
+    if request.method == 'POST':
+        newPenontonUUID = uuid.uuid4()
+        username = request.POST.get("usernameField")
+        password = request.POST.get("passwordField")
+        nama_depan = request.POST.get("firstNameField")
+        nama_belakang = request.POST.get("lastNameField")
+        nomor_hp = request.POST.get("phoneNumberField")
+        email = request.POST.get("emailField")
+        alamat = request.POST.get("addressField")
+        status = request.POST.get("statusField")
+
+        with connection.cursor() as cursor:
+            cursor.execute("SET SEARCH_PATH TO ULEAGUE;")
+            cursor.execute(f"""
+                INSERT INTO USER_SYSTEM VALUES ('{username}','{password}');
+                INSERT INTO NON_PEMAIN (id_non_pemain, nama_depan, nama_belakang, nomor_hp, email, alamat) VALUES ('{newPenontonUUID}', '{nama_depan}', '{nama_belakang}', '{nomor_hp}', '{email}', '{alamat}');
+                INSERT INTO STATUS_NON_PEMAIN (id_non_pemain, status) VALUES ('{newPenontonUUID}', '{status}');
+                INSERT INTO PENONTON (id_penonton, username) VALUES ('{newPenontonUUID}', '{username}');
+            """)
+
+            response = HttpResponse()
+            response.status_code = 200
+
+        return redirect('authentication:login_page')
+    return render(request, "register_penonton.html")
 
 def login_user_system(request):
     if request.method == 'GET':
@@ -113,6 +185,11 @@ def dashboard(request):
             """)
 
             manajer_tim_data = dict_fetch_all(cursor)
+
+            if not manajer_tim_data:
+                context['manajer_tim_data'] = None
+                return render(request, 'dashboard_manajer.html', context)
+
             context['manajer_tim_data'] = manajer_tim_data[0]
 
             return render(request, 'dashboard_manajer.html', context)
@@ -150,6 +227,11 @@ def dashboard(request):
             """)
 
             penonton_pertandingan_data = dict_fetch_all(cursor)
+
+            if not penonton_pertandingan_data:
+                context['penonton_pertandingan_data'] = None
+                return render(request, 'dashboard_penonton.html', context)
+            
             context['penonton_pertandingan_data'] = penonton_pertandingan_data[0]
 
             return render(request, 'dashboard_penonton.html', context)
@@ -185,6 +267,11 @@ def dashboard(request):
             """)
 
             panitia_rapat_data = dict_fetch_all(cursor)
+
+            if not panitia_rapat_data:
+                context['panitia_rapat_data'] = None
+                return render(request, 'dashboard_panitia.html', context)
+
             context['panitia_rapat_data'] = panitia_rapat_data[0]
 
             return render(request, 'dashboard_panitia.html', context)
