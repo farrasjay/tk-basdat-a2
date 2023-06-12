ddfe48b49e0bac052076846bd668d2f396859263
Farras Hafizhudin <farras.hafizhudin@ui.ac.id>
2023-06-12 21:36:57 +0700

    cors fix on deployment #4

--- project_django/settings.py
+++ project_django/settings.py
@@ -28,10 +28,12 @@ SECRET_KEY = 'django-insecure-3@5wx%(^*zl68l(o$^m-3%cvjv0g&mom1ra=oj5f048_al57s7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOST = ["*"]
+
 CSRF_TRUSTED_ORIGINS = [
-        'localhost',
+        'http://localhost/',
         'https://web-production-402e.up.railway.app/',
-        '127.0.0.1',
+        'http://127.0.0.1/',
     ]
 
 # Application definition
