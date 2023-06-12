d4b87421bb10e78ccda36120df84c4634d998306
Farras Hafizhudin <farras.hafizhudin@ui.ac.id>
2023-06-12 21:26:31 +0700

    cors fix on deployment #2

--- project_django/settings.py
+++ project_django/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
         'localhost',
-        'web-production-402e.up.railway.app',
+        'https://web-production-402e.up.railway.app/',
         '127.0.0.1',
     ]
 
